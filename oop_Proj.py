01f06432c53ae14e5efcd7df64dbdbe289619f43
arnab-003 <a.debpro2@gmail.com>
2026-06-07 10:38:11 +0530

    Sign up and Sign in method created

--- oop_Proj.py
+++ oop_Proj.py
@@ -5,6 +5,7 @@ class chatbook:
         self.loggedIn=False
         self.menu()
 
+#creating Menu for users
     def menu(self):
         user_input=input("""Wellcome to ChatBook. How would you like to proced??
                          1. Press 1 for Signup
@@ -12,6 +13,43 @@ class chatbook:
                          3.Press 3 for Write a Post
                          4. Press 4 for Message your friends
                          5.Press any other key to exit!!""")
+        if user_input=="1":
+            self.signup()
+        elif  user_input=="2":
+            self.signin()
+        elif  user_input=="3":
+            pass
+        elif  user_input=="4":
+            pass
+        else:
+            exit()
+    
+    #signup method
+    def signup(self):
+        email=input("Please Enter your valid Email ID -> ")
+        pwd=input("Enter your Password ->")
+        self.username=email
+        self.password=pwd
+        print("You have Signed Up Successfully.")
+        print("\n")
+        self.menu()
+    
+    #signin method
+    def signin(self):
+        if self.username=="" and self.password=="":
+            print("Press 1 for Signup first!!")
+        else:
+            eid=input("Enter your Email Id -> ")
+            pswd=input("Enter your Password -> ")
+            if self.username ==eid and self.password ==pswd:
+                print("You have Successfully Logged in.") 
+                self.loggedIn=True   
+            else:
+                print("Please Enter Valid Credentials!!")
+        print("\n")
+        self.menu()     
+
+        
     
 obj=chatbook()
         
